6aafe415a7ce21d9f14a1b42fc728775b4351d18
freedom97 <claudia.fernandez1@correo.icesi.edu.co>
2020-09-22 14:38:01 -0500

    Docs agregado

---
+++ docs/precache-manifest.b93452bc8424954f9ace2cb9e7a16d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95f1b73f90565c21f8a69422dcabf61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10e5000ae33bae7ae3dc",
+    "url": "/static/css/main.9bdf4420.chunk.css"
+  },
+  {
+    "revision": "c148df94a9d083842dcc",
+    "url": "/static/js/2.fa9c1b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fa9c1b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e5000ae33bae7ae3dc",
+    "url": "/static/js/main.da1145aa.chunk.js"
+  },
+  {
+    "revision": "055f3e008571789170d2",
+    "url": "/static/js/runtime-main.3e4d2dea.js"
+  }
+]);
